d4dfbcf20b7324a51c010e858277e87336627bf7
Jahazielmr <Jahazielmr97@gmail.com>
2017-05-18 18:40:03 -0600

    Comenzando el trabajo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,59 @@
-#include <iostream>
+#include<string>
+#include<iostream>
 
 using namespace std;
 
+
 int main(){
 	
-	return 0;
+	char resp ='s';
+        int option;
+        while(resp=='s'||resp=='S'){
+                cout <<"Menu"<<endl;
+                cout <<"1-Agregar Familias"<<endl;
+                cout <<"2-Agregar Ejercitos"<<endl;
+		cout <<"3-Listar Ejercitos"<<endl;
+		cout <<"4-Modificar Ejercitos"<<endl;
+		cout <<"5-Eliminar Ejercitos"<<endl;
+		cout <<"6-Simular Batallas"<<endl;
+		
+                cin>> option;
+
+                switch (option){
+                        case 1:{
+                            
+
+
+
+
+
+                        break;
+                        }
+
+                        case 2:{
+                               
+
+	
+	 		 break;
+
+                          }
+                }
+                cout<<"Desea continuar? S o N"<<endl;
+                cin >> resp;
+        }
+
+        cout<<"Bye";
+
+return 0;
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+return 0;
 }
